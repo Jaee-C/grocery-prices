dc7b1da5cfe1a9c78afdb4ad5fbab4dbd29457fe
Daniel Chin <77240633+Jaee-C@users.noreply.github.com>
2025-02-08 19:50:44 +1100

    fix gha

--- infra/lambdas/remove_grocery_item.tf
+++ infra/lambdas/remove_grocery_item.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_function" "remove_grocery_item" {
   role          = var.execution_role_arn
   image_uri     = var.grocery_prices_image
   package_type  = "Image"
-  runtime       = "python3.13"
+  memory_size   = 3000
 
   image_config {
     command = ["grocery_prices.handlers.remove_grocery_item.lambda_handler"]
