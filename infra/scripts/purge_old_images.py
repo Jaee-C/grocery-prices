7dd4b92428525d428804af3ffbcb9254265ea15f
Daniel Chin <77240633+Jaee-C@users.noreply.github.com>
2025-02-09 12:17:23 +1100

    fix purge ecr script

--- infra/scripts/purge_old_images.py
+++ infra/scripts/purge_old_images.py
@@ -6,7 +6,7 @@ from mypy_boto3_ecr_public.type_defs import ImageIdentifierTypeDef
 def get_old_images(ecr_client: ECRPublicClient, repository_name) -> list[ImageIdentifierTypeDef]:
     images = ecr_client.describe_images(repositoryName=repository_name)["imageDetails"]
     images.sort(key=lambda x: x["imagePushedAt"] if "imagePushedAt" in x else 0)
-    return [{"imageDigest": image["imageDigest"]} for image in images[:5] if "imageDigest" in image]
+    return [{"imageDigest": image["imageDigest"]} for image in images[:-5] if "imageDigest" in image]
 
 
 def purge_old_images(ecr_client: ECRPublicClient, repository_name, images: list[ImageIdentifierTypeDef]):
