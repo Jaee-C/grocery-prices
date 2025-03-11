4a868d68cdbcb4e082adb4e1c923cf5ca5cbb0cb
Daniel Chin <77240633+Jaee-C@users.noreply.github.com>
2025-03-11 20:06:56 +1100

    add vercel oidc for aws access

--- setup-local.sh
+++ setup-local.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 poetry build-lambda
-localstack start -d
 cd infra || exit
 tflocal apply -auto-approve -var-file=local.tfvars
--- ui/src/server/client.ts
+++ ui/src/server/client.ts
@@ -1,9 +1,14 @@
 import { LambdaClient } from "@aws-sdk/client-lambda";
+import { awsCredentialsProvider }  from "@vercel/functions/oidc";
+
+const AWS_ROLE_ARN = process.env.AWS_ROLE_ARN!;
 
 export const client = new LambdaClient({
   endpoint: process.env.NODE_ENV === "production" ? undefined : "http://localhost:4566",
   region: "us-east-1",
-  credentials: process.env.NODE_ENV === "production" ? undefined : {
+  credentials: process.env.NODE_ENV === "production" ? awsCredentialsProvider({
+    roleArn: AWS_ROLE_ARN,
+  }) : {
     accessKeyId: 'test',
     secretAccessKey: 'test',
   }
