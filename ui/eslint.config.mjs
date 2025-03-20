712e4515c10c82996fee3136e763c8507edd15ef
Daniel Chin <77240633+Jaee-C@users.noreply.github.com>
2025-03-20 23:59:33 +1100

    fix linting

--- ui/eslint.config.mjs
+++ ui/eslint.config.mjs
@@ -1,7 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import stylisticJs from '@stylistic/eslint-plugin-js';
+import stylisticJs from "@stylistic/eslint-plugin-js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
